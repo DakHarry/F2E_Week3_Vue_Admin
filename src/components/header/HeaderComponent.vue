dac93602502aefdf70cb5b3a8b4e9416c7a0b7d1
harry-chan <harry.chan@soohoobook.com>
2018-06-22 18:38:55 +0800

    add Header & Router

--- src/components/header/HeaderComponent.vue
+++ src/components/header/HeaderComponent.vue
@@ -1,8 +1,18 @@
 <template>
-    <div>Hi</div>
+    <div id="header">
+        <div class="container">
+            <div class="logo">ShopTime</div>
+            <div class="menu">
+                <a class="active" href="">Home</a>
+                <a href="">Order</a>
+                <a href="">Product</a>
+            </div>
+            <div class="user">Admin</div>
+        </div>
+    </div>
 </template>
 <script>
 export default {
-
+  name: 'Header'
 }
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import App from '@/App'
+import Home from '../components/home/HomeComponent.vue'
+import Order from '../components/order/OrderComponent.vue'
+import Product from '../components/product/ProductComponent.vue'
 
 Vue.use(Router)
 
@@ -8,8 +10,18 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'App',
-      component: App
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/Order',
+      name: 'Order',
+      component: Order
+    },
+    {
+      path: '/Product',
+      name: 'Product',
+      component: Product
     }
   ]
 })
